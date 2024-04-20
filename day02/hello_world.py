4fcb495ec817dd8b8a6125b22e79499026d06a2e
LironH <liron151@gmail.com>
2024-04-20 16:52:52 +0300

    new code

--- day02/hello_world.py
+++ day02/hello_world.py
@@ -1,3 +1,17 @@
-print("Hello World")
-name=input("Welcome, write your name:")
-print("Hello, " + name)
+import random
+
+number=random.randint(1,21)
+count=1
+while True: 
+    guess=int(input("Guess a number: "))
+    if guess==number:
+        print("You guessed correctly")
+        break
+    elif guess>number:
+        print("Your number is too high")
+    else: 
+        print("Your number is too low")
+    count+=1
+print (f"You guessed {count} times")
+
+    
