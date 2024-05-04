4d9d56aa6dec8dc50fca70d3af6a65c0882a7973
LironH <liron151@gmail.com>
2024-05-04 18:44:59 +0300

    add new files

---
+++ day02/hello_world.py
@@ -0,0 +1,17 @@
+import random
+
+number=random.randint(1,21)
+count=1
+while True: 
+    guess = int(input("Guess a number: "))
+    if guess==number:
+        print("You guessed correctly")
+        break
+    elif guess>number:
+        print("Your number is too high")
+    else: 
+        print("Your number is too low")
+    count+=1
+print (f"You guessed {count} times")
+
+    
