63a857af72d15cb34148f734551d9edea0919ad0
LironH <liron151@gmail.com>
2024-05-31 10:26:17 +0300

    changes in assignment 6 test

--- day06/test_convert_data.py
+++ day06/test_convert_data.py
@@ -1,18 +1,21 @@
 from convert_data import convert_file
-import pytest
 
 #The test will check the Gene expression column to see if the calculation was done correctly 
 
+
 def test_calculate_gene_expression():
     df_res = convert_file("test.csv")
-    df1 = df_res.iloc[1]
-    col = df1['Gene Expression']
-    assert(col, 0.00000049273705977)
+    df1 = df_res.iloc[0]
+    col1 = df1['Gene Expression']
+    assert col1==2**-20.953
+
 
     df2 = df_res.iloc[2]
-    col = df2['Gene Expression']
-    assert(col,0.000000274015319320)
+    col2 = df2['Gene Expression']
+    assert col2== 2**-21.799
 
 
 
-test_calculate_gene_expression()
+
+
+
